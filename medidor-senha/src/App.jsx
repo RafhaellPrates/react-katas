0bd251ec1f1edeb683a79fcd8cdaae8434293cef
Rafhael Prates <fellepy960@gmail.com>
2026-06-22 20:13:08 -0300

    feat: adiciona 5  Criterios de seguranca
    Closes #2

--- medidor-senha/src/App.jsx
+++ medidor-senha/src/App.jsx
@@ -1,26 +1,59 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './index.css'
 
 function App() {
 
-  const [senha ,setSenha] = useState('')
+  const [ senha ,setSenha ] = useState('')
+  
+  
+  function valida(senha){
 
+    let count = false,
+     menor = false ,
+     maior= false, 
+     especial= false, 
+     numero= false
+    
+    
+    if(senha.length >=8){
+      count = true
+    }
+    if(senha.match(/[a-z]/)){
+      menor = true
+    }
+    if(senha.match(/[A-Z]/)){
+      maior = true
+    }
+    if(senha.match(/[0-9]/)){
+      numero = true
+    }
+    if(senha.match(/[!@#$%&?*]/)){
+      especial = true
+    }
+    
+    let state ={count,menor,maior,numero,especial}
+    return (state)
+  }
+  
   function inputChange(event){
     setSenha(event.target.value)
-
+    
+    valida(senha)
   }
   
   return (
 
-    
-    <main>
-      <h1>Medidor de Força de Senha</h1>
-        <div>
-          <p>Senha: <input value={senha} onChange={inputChange} /></p>
-          
-        </div>
+    <>
+      <main>
+        <h1>Medidor de Força de Senha</h1>
+          <div>
+            <p>Senha: <input value={senha} onChange={inputChange} /></p>
+            
+        
+          </div>
 
-    </main>
+      </main>
+    </>
   )
 }
 
